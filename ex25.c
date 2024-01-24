a7d067244fd4864762eaa17977ee6ca5de429e42
rebeccadimarco <157354203+rebeccadimarco@users.noreply.github.com>
2024-01-24 16:00:53 +0000

    giorno 3

---
+++ ex25.c
@@ -0,0 +1,7 @@
+int voti[5]={5, 6, 4, 2, 7};
+//           0  1  2  3  4  = indici dell' arrei
+printf(" %d", voti[2]);
+voti[3]=5;
+//   piano che voglio cambiare(voglio modificare il 2) 5= nuovo voto che cambio invece che 2
+char parola[5]= {'c', 'i', 'a', 'o','\0'}
+// \0 = terminatore stringa 
